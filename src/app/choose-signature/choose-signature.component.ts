ce2de623880c98440a93a987bc317c0d2339cc12
Dolly <dolly@Dollys-Mac-mini.local>
2020-04-21 20:12:07 +0530

    created img without package

--- src/app/choose-signature/choose-signature.component.ts
+++ src/app/choose-signature/choose-signature.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { Subject } from 'rxjs';
 import { debounceTime } from 'rxjs/operators';
 import * as textToImage from 'text-to-image';
@@ -9,6 +9,8 @@ import * as textToImage from 'text-to-image';
 })
 export class ChooseSignatureComponent implements OnInit {
   name: string;
+  @ViewChild('sigPad') sigPad: ElementRef;
+
   initial: string;
   currentFontFamily: string;
   fontFamily = [{ name: 'Tangerine', url: 'https://fonts.googleapis.com/css?family=Tangerine' }, { name: 'Dancing Script, cursive', url: 'https://fonts.googleapis.com/css2?family=Dancing+Script' }];
@@ -27,20 +29,31 @@ export class ChooseSignatureComponent implements OnInit {
   }
 
   onCreate() {
-    const options = {
-      maxWidth: 1000,
-      fontSize: 48,
-      fontFamily: this.currentFontFamily,
-      lineHeight: 50,
-      margin: 5,
-      textColor: "Black"
-    }
-    textToImage.generate(this.name, options).then((dataUri) => {
-      this.nameImg = dataUri;
-    });
+    // const options = {
+    //   maxWidth: 1000,
+    //   fontSize: 48,
+    //   fontFamily: this.currentFontFamily,
+    //   lineHeight: 50,
+    //   margin: 5,
+    //   textColor: "Black"
+    // }
+    // textToImage.generate(this.name, options).then((dataUri) => {
+    //   this.nameImg = dataUri;
+    // });
 
-    textToImage.generate(this.initial, options).then((dataUri) => {
-      this.initialImg = dataUri;
-    });
+    // textToImage.generate(this.initial, options).then((dataUri) => {
+    //   this.initialImg = dataUri;
+    // });
+
+    const sigPadElement = this.sigPad.nativeElement;
+    const signContext = sigPadElement.getContext('2d');
+    signContext.font = `normal 48px ${this.currentFontFamily}`;
+    signContext.canvas.width = signContext.measureText(this.name).width + 30;
+    signContext.canvas.lineHeight = signContext.measureText(this.name).lineHeight;
+
+    signContext.font = `normal 48px ${this.currentFontFamily}`;
+    signContext.fillStyle = 'black';
+    signContext.fillText(this.name, 10, 100);
+    this.nameImg = signContext.canvas.toDataURL()
   }
 }
